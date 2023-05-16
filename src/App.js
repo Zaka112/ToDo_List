a7c64ecd4d46d85dfd35ece9a84ae9412cd0d07b
Muhammad Zaka Ullah <97341177+Zaka112@users.noreply.github.com>
2023-05-16 21:42:13 +0200

    Style and requested changes

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   const [list, setList] = useState([]);
   const [input, setInput] = useState("");
   const [date, setDate] = useState("");
-  const [progress, setProgress] = useState("not-started");
+  const [progress, setProgress] = useState("Choose a status");
 
-  const addTodo = (todo) => {
+  function addTodo(todo) {
     const newTodo = {
       id: Math.random(),
       todo: todo,
@@ -17,11 +17,13 @@ function App() {
     };
     if (!todo) alert("Title required");
     else if (!date) alert("Date required!");
+    else if (progress === "Choose a status") alert("Choose a Status!");
     else setList([...list, newTodo]);
+
     setInput("");
     setDate("");
-    setProgress("not-started");
-  };
+    setProgress("Choose a status");
+  }
 
   function deletTodo(id) {
     const newList = list.filter((todo) => todo.id !== id);
@@ -29,42 +31,46 @@ function App() {
   }
 
   return (
-    <div>
-      <h1>to do list</h1>
-      <lable>Title:</lable>{" "}
-      <input
-        type="text"
-        placeholder="Title"
-        required
-        value={input}
-        onChange={(e) => {
-          setInput(e.target.value);
-        }}
-      />
-      <div>
-        {" "}
-        <lable>Date:</lable>{" "}
+    <div className="App">
+      <form>
+        <label>Title:</label>{" "}
         <input
-          type="date"
-          value={date}
+          type="text"
+          placeholder="Title"
+          required
+          value={input}
           onChange={(e) => {
-            setDate(e.target.value);
+            setInput(e.target.value);
           }}
-        ></input>
-      </div>
-      <label for="progress">Progress:</label>
-      <select value={progress} onChange={(e) => setProgress(e.target.value)}>
-        <option value="in-progress">in-progress</option>
-        <option value="done">done</option>
-        <option value="not-started">not-started</option>
-      </select>
+        />
+        <div>
+          {" "}
+          <label>Date:</label>
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+          ></input>
+        </div>
+        <label>Progress:</label>
+        <select value={progress} onChange={(e) => setProgress(e.target.value)}>
+          <option>Choose a status</option>
+          <option value="in-progress">in-progress</option>
+          <option value="done">done</option>
+          <option value="not-started">not-started</option>
+        </select>
+      </form>
       <br></br>
-      <button onClick={() => addTodo(input, date, progress)}>Submit</button>
+      <button onClick={() => addTodo(input)}>Submit</button>
       <div className="parent">
+        <h1>to do list</h1>
         {list.map((todo) => {
           return (
             <div key={todo.id} className="hight">
-              {todo.todo} {todo.date} {todo.progress}
+              <p className={todo.progress}></p> <p>{todo.todo}</p>{" "}
+              <p>{todo.date}</p>{" "}
               <button onClick={() => deletTodo(todo.id)}>Delete</button>
             </div>
           );
--- src/components/AddEvent.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function AddEvent() {
-  return (
-    <div>AddEvent</div>
-  )
-}
