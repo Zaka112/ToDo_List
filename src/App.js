57e50ea1acd471772ee3fd3d628cb03bcc827beb
Muhammad Zaka Ullah <97341177+Zaka112@users.noreply.github.com>
2023-05-16 02:03:41 +0200

    add remove but not color

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+  const [date, setDate] = useState("");
+  const [progress, setProgress] = useState("not-started");
+
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+      date: date,
+      progress: progress,
+    };
+    if (!todo) alert("Title required");
+    else if (!date) alert("Date required!");
+    else setList([...list, newTodo]);
+    setInput("");
+    setDate("");
+    setProgress("not-started");
+  };
+
+  function deletTodo(id) {
+    const newList = list.filter((todo) => todo.id !== id);
+    setList(newList);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>to do list</h1>
+      <lable>Title:</lable>{" "}
+      <input
+        type="text"
+        placeholder="Title"
+        required
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
+      <div>
+        {" "}
+        <lable>Date:</lable>{" "}
+        <input
+          type="date"
+          value={date}
+          onChange={(e) => {
+            setDate(e.target.value);
+          }}
+        ></input>
+      </div>
+      <label for="progress">Progress:</label>
+      <select value={progress} onChange={(e) => setProgress(e.target.value)}>
+        <option value="in-progress">in-progress</option>
+        <option value="done">done</option>
+        <option value="not-started">not-started</option>
+      </select>
+      <br></br>
+      <button onClick={() => addTodo(input, date, progress)}>Submit</button>
+      <div className="parent">
+        {list.map((todo) => {
+          return (
+            <div key={todo.id} className="hight">
+              {todo.todo} {todo.date} {todo.progress}
+              <button onClick={() => deletTodo(todo.id)}>Delete</button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
