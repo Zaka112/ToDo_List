413acc8b2fa9997a3fa738e616b86ca6d74e8726
Muhammad Zaka Ullah <97341177+Zaka112@users.noreply.github.com>
2023-12-26 20:40:00 +0100

    alert and toaster added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 
+import { toast } from "react-toastify";
+import { confirmAlert } from "react-confirm-alert";
+
 import "./App.css";
 
 function App() {
   const [list, setList] = useState([]);
   const [input, setInput] = useState("");
   const [date, setDate] = useState("");
-  const [progress, setProgress] = useState("Choose a status");
+  const [progress, setProgress] = useState("Status");
 
   function addTodo(todo) {
     const newTodo = {
@@ -15,14 +18,55 @@ function App() {
       date: date,
       progress: progress,
     };
-    if (!todo) alert("Title required");
-    else if (!date) alert("Date required!");
-    else if (progress === "Choose a status") alert("Choose a Status!");
-    else setList([...list, newTodo]);
-
-    setInput("");
-    setDate("");
-    setProgress("Choose a status");
+    if (!todo) {
+      confirmAlert({
+        title: "Tittle required",
+        message: "Please write title for todo item.",
+        buttons: [
+          {
+            label: "OK",
+            color: "#fffccc",
+          },
+        ],
+      });
+    } else if (!date) {
+      confirmAlert({
+        title: "Date required",
+        message: "Please select a date for todo item.",
+        buttons: [
+          {
+            label: "OK",
+            color: "#fffccc",
+          },
+        ],
+      });
+    } else if (progress === "Status") {
+      confirmAlert({
+        title: "Status required",
+        message: "Please select a status for todo item.",
+        buttons: [
+          {
+            label: "OK",
+            color: "#fffccc",
+          },
+        ],
+      });
+    } else {
+      setList([...list, newTodo]);
+      setInput("");
+      setDate("");
+      setProgress("Status");
+      toast.success(`${newTodo.todo} todo added`, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
   }
 
   function deletTodo(id) {
@@ -78,7 +122,28 @@ function App() {
               <div className="width">{todo.date}</div>
               <div className="width">
                 {" "}
-                <button onClick={() => deletTodo(todo.id)}>Delete</button>
+                #TODO: Edit todo
+                <button>Edit</button>
+                <button
+                  onClick={() =>
+                    confirmAlert({
+                      title: "Confirm to Proceed",
+                      message: "Are you sure to remove todo?",
+                      buttons: [
+                        {
+                          label: "Yes",
+                          onClick: () => deletTodo(todo.id),
+                          color: "#fffccc",
+                        },
+                        {
+                          label: "No",
+                        },
+                      ],
+                    })
+                  }
+                >
+                  Delete
+                </button>
               </div>
             </div>
           );
--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "react-confirm-alert/src/react-confirm-alert.css";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <ToastContainer />
   </React.StrictMode>
 );
 
